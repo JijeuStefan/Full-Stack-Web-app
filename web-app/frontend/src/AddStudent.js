1ee3a3641d7fc7f37c09b7ad86c521e600eefbf6
Stefan-code-eng1 <jijeus@gmail.com>
2024-05-18 15:09:41 +0300

    fix
    improved the update and add

--- web-app/frontend/src/AddStudent.js
+++ web-app/frontend/src/AddStudent.js
@@ -11,12 +11,15 @@ function AddStudent() {
 
     function handleSubmit(event){
         event.preventDefault();
+        if (name === "" || email === "" || group === "")
+            alert("All fields should be completed!");
+        else{
         axios.post('http://localhost:8081/student/add', {name, email, group})
         .then(res => {
             console.log(res);
             navigate('/');
         }).catch(err => console.log(err));
-    }
+    }}
 
     return(
         <div className='d-flex vh-100 bg-primary justify-content-center align-items-center'>
