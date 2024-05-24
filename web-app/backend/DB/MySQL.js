eb77942a02574acd8fba7b51bc148ad1212cf280
Stefan-code-eng1 <jijeus@gmail.com>
2024-05-24 08:43:32 +0300

    fix
    did the db with .env

--- web-app/backend/DB/MySQL.js
+++ web-app/backend/DB/MySQL.js
@@ -1,11 +1,13 @@
+require('dotenv').config();
+
 function StartDB(){
     const mysql = require("mysql");
 
     const db = mysql.createConnection({
-        host: "localhost",
-        user: "root",
-        password: "",
-        database: "university"
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
     })
 
     return db;
