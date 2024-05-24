222d050333eea7ab1393ea8733653da705c04067
Stefan-code-eng1 <jijeus@gmail.com>
2024-05-24 13:57:32 +0300

    done

--- web-app/backend/Service/Register.js
+++ web-app/backend/Service/Register.js
@@ -89,12 +89,12 @@ function RegisterService(app, db) {
                 }
 
                 try {
-                    // const hashedPassword = await bcrypt.hash(req.body.password, 5);
+                    const hashedPassword = await bcrypt.hash(req.body.password, 5);
                     const sql = "INSERT INTO `credentials` (`Name`, `Email`, `Password`, 'Token') VALUES (?)";
                     const values = [
                         req.body.name,
                         req.body.email,
-                        req.body.password,
+                        hashedPassword,
                         "empty"
                     ]
                 
